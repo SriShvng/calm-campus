53a1e7c7cc76deb86e86fe1f246d5a84edc9bd5a
Saarthak09 <saarthaksingh09@gmail.com>
2023-11-30 17:01:13 -0500

    instuctor ids added

--- lib/instructor.ts
+++ lib/instructor.ts
@@ -1,3 +1,3 @@
 export const isInstructor = (userId?: string | null) => {
-    return userId === process.env.NEXT_PUBLIC_INSTRUCTOR_ID;
+    return (userId === process.env.NEXT_PUBLIC_INSTRUCTOR_ID1 || userId === process.env.NEXT_PUBLIC_INSTRUCTOR_ID2 || userId === process.env.NEXT_PUBLIC_INSTRUCTOR_ID3);
   }
