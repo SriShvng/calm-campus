aae7eb912337e02aa09db064af571ea7a065e6da
Saarthak09 <saarthaksingh09@gmail.com>
2023-11-30 15:46:54 -0500

    course deletion modified

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -8,7 +8,6 @@ import { Separator } from "@/components/ui/separator";
 import { Preview } from "@/components/preview";
 
 import { VideoPlayer } from "./_components/video-player";
-import { CourseEnrollButton } from "./_components/course-enroll-button";
 import { CourseProgressButton } from "./_components/course-progress-button";
 
 const ChapterIdPage = async ({
@@ -59,7 +58,6 @@ const ChapterIdPage = async ({
             courseId={params.courseId}
             nextChapterId={nextChapter?.id}
             playbackId={muxData?.playbackId!}
-            // playbackId="I01xjZKlSR7IU1vgpE5FKkQ9bbq7eDi48zg6UTz5Xe54"
             completeOnEnd={completeOnEnd}
           />
         </div>
@@ -68,7 +66,6 @@ const ChapterIdPage = async ({
             <h2 className="text-2xl font-semibold mb-2">
               {chapter.title}
             </h2>
-            {/* {enroll ? ( */}
               <CourseProgressButton
                 chapterId={params.chapterId}
                 courseId={params.courseId}
@@ -107,76 +104,3 @@ const ChapterIdPage = async ({
 }
  
 export default ChapterIdPage;
-
-
-
-
-// import { auth } from "@clerk/nextjs";
-// import { redirect } from "next/navigation";
-// import { File } from "lucide-react";
-
-// import { getChapter } from "@/actions/get-chapter";
-// import { Banner } from "@/components/banner";
-// import { Separator } from "@/components/ui/separator";
-// import { Preview } from "@/components/preview";
-
-// import { VideoPlayer } from "./_components/video-player";
-// import { CourseEnrollButton } from "./_components/course-enroll-button";
-
-// const ChapterIdPage = async ({
-//   params
-// }: {
-//   params: { courseId: string; chapterId: string }
-// }) => {
-//   const { userId } = auth();
-  
-//   if (!userId) {
-//     return redirect("/");
-//   } 
-
-//   const {
-//     chapter,
-//     course,
-//     muxData,
-//     assignments,
-//     nextChapter,
-//     userProgress,
-//   } = await getChapter({
-//     userId,
-//     chapterId: params.chapterId,
-//     courseId: params.courseId,
-//   });
-
-//   if (!chapter || !course) {
-//     return redirect("/")
-//   }
-
-
-//   const completeOnEnd =  !userProgress?.isCompleted;
-
-//   return ( 
-//     <div>
-//       {userProgress?.isCompleted && (
-//         <Banner
-//           variant="success"
-//           label="You already completed this chapter."
-//         />
-//       )}
-//       {/* <div className="flex flex-col max-w-4xl mx-auto pb-20"> */}
-//         {/* <div className="p-4"> */}
-//           <VideoPlayer
-//             chapterId={params.chapterId}
-//             title={chapter.title}
-//             courseId={params.courseId}
-//             nextChapterId={nextChapter?.id}
-//             playbackId="I01xjZKlSR7IU1vgpE5FKkQ9bbq7eDi48zg6UTz5Xe54"
-//             completeOnEnd={completeOnEnd}
-//           />
-//         {/* </div> */}
-        
-//       {/* </div> */}
-//     </div>
-//    );
-// }
- 
-// export default ChapterIdPage;
--- app/(dashboard)/(routes)/instructor/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/instructor/courses/[courseId]/page.tsx
@@ -1,7 +1,4 @@
-// "use client"
-import { auth, clerkClient } from "@clerk/nextjs";
-import { useState } from "react";
-import { useRouter } from "next/router";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { LayoutDashboard, ListChecks, File } from "lucide-react";
 
@@ -15,26 +12,12 @@ import { ChaptersForm } from "./_components/chapters-form";
 import { Banner } from "@/components/banner";
 import { Actions } from "./_components/actions";
 
-const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+const CourseIdPage = async ({
+  params
+}: {
+  params: { courseId: string }
+}) => {
   const { userId } = auth();
-  let userNameList;
-
-  try {
-    const enrolledUsers = await db.enroll.findMany({
-      where: {
-        courseId: params.courseId,
-      },
-      select: {
-        userId: true,
-      },
-    });
-
-    const userId = enrolledUsers.map((enrollment) => enrollment.userId);
-
-    userNameList = await clerkClient.users.getUserList({ userId });
-
-    console.log({ userNameList });
-  } catch {}
 
   if (!userId) {
     return redirect("/");
@@ -43,7 +26,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
-      userId,
+      userId
     },
     include: {
       chapters: {
@@ -62,90 +45,82 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     course.title,
     course.description,
     course.imageUrl,
-    course.chapters.some((chapter) => chapter.isPublished),
+    course.chapters.some(chapter => chapter.isPublished),
   ];
 
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
 
-  const completionText = `(${completedFields}/${totalFields})`;
+  const completionText = `(${completedFields}/${totalFields})`
 
   const isComplete = requiredFields.every(Boolean);
 
-  return (
+
+  return ( 
     <>
       {!course.isPublished && (
-        <Banner label="This course is unpublished. It will not be visible to the students." />
+        <Banner
+          label="This course is unpublished. It will not be visible to the students."
+        />
       )}
-      <div className="p-6">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col gap-y-2">
-            <h1 className="text-2xl font-medium">Course setup</h1>
-            <span className="text-sm text-slate-700">
-              Complete all fields {completionText}
-            </span>
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">
+            Course setup
+          </h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+        <Actions
+          disabled={!isComplete}
+          courseId={params.courseId}
+          isPublished={course.isPublished}
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">
+              Customize your course
+            </h2>
           </div>
-          <Actions
-            disabled={!isComplete}
-            courseId={params.courseId}
-            isPublished={course.isPublished}
+          <TitleForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <DescriptionForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <ImageForm
+            initialData={course}
+            courseId={course.id}
           />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={LayoutDashboard} />
-              <h2 className="text-xl">Customize your course</h2>
-            </div>
-            <TitleForm initialData={course} courseId={course.id} />
-            <DescriptionForm initialData={course} courseId={course.id} />
-            <ImageForm initialData={course} courseId={course.id} />
-          </div>
-          <div className="space-y-6">
+        <div className="space-y-6">
             <div>
-              <div className="flex items-center gap-x-2">
-                <IconBadge icon={ListChecks} />
-                <h2 className="text-xl">Course modules</h2>
-              </div>
-              <ChaptersForm initialData={course} courseId={course.id} />
-              <div></div>
-              <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
-                <div className="font-medium">
-                  <h2 className="font-meidum p-2">Enrolled Students</h2>
-                  <table className="w-full table-auto border-collapse">
-                    <thead>
-                      <tr>
-                        <th className="p-2 px-4  bg-gray-200 text-gray-900 font-medium">
-                          First Name
-                        </th>
-                        <th className="p-2 px-4 bg-gray-200 text-gray-900 font-medium">
-                          Last Name
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {userNameList!.map((user) => (
-                        <tr key={user.id} className="border-b border-gray-200">
-                          <td className="p-2 px-4 text-center text-sm">
-                            {user.firstName}
-                          </td>
-                          <td className="p-2 px-4 text-center text-sm">
-                            {user.lastName}
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
+                <div className="flex items-center gap-x-2">
+                    <IconBadge icon={ListChecks}/>
+                    <h2 className="text-xl">
+                        Course modules
+                    </h2>
                 </div>
-              </div>
+                <ChaptersForm
+                initialData={course}
+                courseId={course.id}
+                />
             </div>
-            <div></div>
+            <div>
           </div>
-          <div></div>
         </div>
       </div>
+    </div>
     </>
-  );
-};
 
+   );
+}
+ 
 export default CourseIdPage;
